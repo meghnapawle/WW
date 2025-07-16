89f965a3efb178d1de39c28df6191e11d0cd8967
Dev Sangam Kumar <get.d3v@gmail.com>
2025-07-16 17:15:19 +0530

    remove unused imports

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './home/App.jsx'
 import Nav from '../components/navbar/Nav.jsx'
 import Items from './home/Items.jsx'
